8f2b543824a5091188a60885e1a3d11bce06587f
Manh <vdmanh107@gmail.com>
2018-07-05 10:25:44 +0700

    chap 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531170931) do
+ActiveRecord::Schema.define(version: 20180705030715) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180531170931) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
