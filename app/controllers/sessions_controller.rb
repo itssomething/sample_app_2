1358fa4398aac78cae5e374eebb9aff1eb0a1a51
Manh <vdmanh107@gmail.com>
2018-07-11 13:12:45 +0700

    chap 12

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by email: params[:session][:email].downcase
-    if @user && @user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       if user.activated?
         log_in user
         params[:session][:remember_me] == "1" ? remember(user) : forget(user)
@@ -20,4 +20,7 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_path
   end
+
+  private
+  attr_reader :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "password_resets/new"
+  get "password_resets/edit"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
